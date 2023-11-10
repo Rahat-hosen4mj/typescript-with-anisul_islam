ffe7f66a99ec18e5f04a24242d0182767fc6f629
Rahat-hosen4mj <mrrahathosen@gmail.com>
2023-11-10 19:49:40 +0600

    learn what is unin-type in typescript

---
+++ SecondProgram/union-type.js
@@ -0,0 +1,5 @@
+var userAccountNum = '34';
+function displayUserId(id) {
+    console.log("User Id is: ".concat(id));
+}
+displayUserId(454);
---
+++ SecondProgram/union-type.ts
@@ -0,0 +1,6 @@
+let userAccountNum: (string | number) = '34'
+
+function displayUserId(id: number|string): void{
+    console.log(`User Id is: ${id}`)
+}
+displayUserId(454)
